a852692061a589e7d803766c78868e5083948884
Nauka <israr.ru@yandex.ru>
2023-10-02 22:37:33 +0700

    - при клике на кнопку toolbar записываем новое состояние в authenticated и переходим на страницу логина

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,19 +1,28 @@
 // src/components/CustomToolbar.js
-import {AppBar, Button, Typography} from "@mui/material";
+import {AppBar, Button, Toolbar, Typography} from "@mui/material";
 import React from 'react';
+import {useHistory} from "react-router-dom";
 
-const Toolbar = () => {
+const AppToolbar = () => {
+
+    const history = useHistory();
+    const logout = () => {
+        // Сохраните состояние авторизации в localStorage
+        localStorage.setItem('authenticated', 'false');
+        history.push('/login');
+    }
+
     return (
         <AppBar position="static">
             <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
                     Your App Name
                 </Typography>
-                <Button color="inherit">Logout</Button>
+                <Button color="inherit" onClick={() => logout()}>Logout</Button>
             </Toolbar>
         </AppBar>
     );
 };
 
 
-export default Toolbar
+export default AppToolbar
